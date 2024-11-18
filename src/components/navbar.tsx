4464d1ec28532da6dc6c2d1e05eba6b5295d3296
fikrianggara <fikriseptriananggara@gmail.com>
2024-11-18 09:26:20 +0700

    fix navbar font size

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,19 +6,28 @@ const Navbar = () => {
     <div className="sticky top-0 z-30 text-white w-full justify-end backdrop-blur md:backdrop-blur-none">
       <ul className="flex space-x-4 text-sm items-center justify-end h-8 p-8 md:w-[20rem] place-self-end">
         <li className="flex-1">
-          <Link href="/#hero" className="flex flex-col group text-base">
+          <Link
+            href="/#hero"
+            className="flex flex-col group text-sm md:text-base"
+          >
             About{" "}
             <span className="w-0 group-hover:w-3/4 duration-300 ease-in-out h-0.5 bg-green-500"></span>
           </Link>
         </li>
         <li className="flex-1">
-          <Link href="/#project" className="flex flex-col group text-base">
+          <Link
+            href="/#project"
+            className="flex flex-col group text-sm md:text-base"
+          >
             Projects{" "}
             <span className="w-0 group-hover:w-3/4 duration-300 ease-in-out h-0.5 bg-green-500 "></span>
           </Link>
         </li>
         <li className="flex-1">
-          <Link href="/#contact" className="flex flex-col group text-base">
+          <Link
+            href="/#contact"
+            className="flex flex-col group text-sm md:text-base"
+          >
             Contact{" "}
             <span className="w-0 group-hover:w-3/4 duration-300 ease-in-out h-0.5 bg-green-500"></span>
           </Link>
